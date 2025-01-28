e8115ec308b231fe622add84eb8249a3224de00f
Asuka Minato <i@asukaminato.eu.org>
2025-01-29 02:59:51 +0900

    try

--- lib/example.dart
+++ lib/example.dart
@@ -6,7 +6,7 @@ Future<void> example() async {
   await RustLib.init();
   
   // Create a new FSRS instance with default parameters
-  final fsrs = await FSRS.new(
+  final fsrs = await api.FSRS.new(
     parameters: List<double>.from([
       0.4, 0.6, 2.4, 5.8, 4.93, 0.94, 0.86, 0.01, 1.49, 0.14, 0.94,
       2.18, 0.05, 0.34, 1.26, 0.29, 2.61, 0.05, 0.4
